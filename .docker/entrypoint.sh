07bcbae11d706569fafce587d72b9ecb96ac6cde
이건희 <qwertys1347@gmail.com>
2025-07-09 17:23:06 +0900

    chore: tistory 앱 설정

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-echo ">>> ENTRYPOINT SCRIPT START <<<"
 cd /app
 
 if [ ! -f "manage.py" ]; then
   django-admin startproject config .
 fi
-
-echo ">>> ENTRYPOINT SCRIPT END <<<"
 
 exec python manage.py runserver 0.0.0.0:8000
