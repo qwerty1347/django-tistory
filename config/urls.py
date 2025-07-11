3799515a7aaba93ce18dc94e8bd885fd0ab4dfaf
이건희 <qwertys1347@gmail.com>
2025-07-11 18:11:23 +0900

    feat: pybo view, template, superuser 테스트

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tistory/', include('tistory.urls')),
+    path('pybo/', include('pybo.urls')),
 ]
